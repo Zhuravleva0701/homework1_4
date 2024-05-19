6aa2c6625f48f4272efc6fa5253fc4377aa45315
Zhuravleva0701 <167981362+Zhuravleva0701@users.noreply.github.com>
2024-05-19 17:02:13 +0300

    Add files via upload

---
+++ homework1_2_4.py
@@ -0,0 +1,5 @@
+import homework1_4 as hw
+
+hw.say_hello()
+
+hw.say_hello_()
---
+++ homework1_4.py
@@ -0,0 +1,10 @@
+def say_hello():
+    print('Hello world!')
+
+
+say_hello()
+
+def say_hello_():
+    print('Привет, мир!')
+
+say_hello_()
